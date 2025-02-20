c34a2d2dac777659d879909a24dd655e24f6e469
Juan Jo Rangel <Juan.Jose.Rangel@ibm.com>
2025-02-20 11:16:02 -0600

    README Updated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,18 @@ import {onRequest} from "firebase-functions/v2/https";
 import express, {Request, Response} from "express";
 import {taskRoutes} from "./routes/tasksRoutes";
 import {usersRoutes} from "./routes/usersRoutes";
+import cors from "cors"; // Importa cors
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.get("/hello", (req : Request, res : Response) => {
   res.send("Tasks");
